d61624d1c5f258628f01e36b719230004d5cac39
Mateo <mateoutn2024@gmail.com>
2026-06-22 18:32:30 -0300

    design(frontend): migrar modulo de perfil a un layout profesional SaaS con grilla de publicaciones

--- Frontend/src/app/features/posts/publications/publications.component.ts
+++ Frontend/src/app/features/posts/publications/publications.component.ts
@@ -8,10 +8,4 @@ import { CommonModule } from '@angular/common';
   templateUrl: './publications.component.html',
   styleUrls: ['./publications.component.scss']
 })
-export class PublicationsComponent {
-  // Estructura de vista requerida para Sprint 1
-  mockPublications = [
-    { title: 'Bienvenidos a la Red', message: 'Este es el espacio inicial de nuestra comunidad.', author: 'Admin' },
-    { title: 'Primeros Pasos en Angular', message: 'Comparto mi emoción usando componentes autónomos.', author: 'Mateo' }
-  ];
-}
+export class PublicationsComponent {}
