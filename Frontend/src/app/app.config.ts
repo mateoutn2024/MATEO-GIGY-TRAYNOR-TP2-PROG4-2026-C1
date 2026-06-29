d0705e4b07397213ac65724fb666c20b13787fb6
Mateo <mateoutn2024@gmail.com>
2026-06-29 16:12:12 -0300

    incorporando PWA estandar

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'; 
 import { HTTP_INTERCEPTORS } from '@angular/common/http'; 
-import { AuthInterceptor } from './core/interceptors/auth.interceptor'; 
+import { AuthInterceptor } from './core/interceptors/auth.interceptor';
+import { provideServiceWorker } from '@angular/service-worker'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +15,9 @@ export const appConfig: ApplicationConfig = {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
       multi: true 
-    }
+    }, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 };
