a32789e23ee29a672c29617a86835f5101f991d2
Mateo <mateoutn2024@gmail.com>
2026-07-01 09:13:09 -0300

    modificando

--- Backend/src/modules/users/users.controller.ts
+++ Backend/src/modules/users/users.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post, Delete, Param, Body, UseGuards, HttpCode, HttpSt
 import { UsersService } from './users.service';
 import { AdminGuard } from '../auth/admin.guard'; 
 import { AuthGuard } from '../auth/auth.guard';
+import { UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('usuarios')
 @UseGuards(AuthGuard, AdminGuard)
@@ -30,5 +32,23 @@ export class UsersController {
   @HttpCode(HttpStatus.OK)
   async rehabilitar(@Param('id') id: string) {
     return this.usersService.cambiarEstado(id, true);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @UseInterceptors(FileInterceptor('avatar')) 
+  async crearUsuarioAdmin(
+    @Body() body: any, 
+    @UploadedFile() file: Express.Multer.File
+  ) {
+    
+    if (file) {
+      body.avatarUrl = `/uploads/avatars/${file.filename}`; 
+    } else {
+      body.avatarUrl = '/uploads/avatars/default.png'; 
+    }
+
+    console.log('-> DATOS RECIBIDOS EN EL BACKEND:', body);
+    return this.usersService.crearDesdeAdmin(body);
   }
 }
